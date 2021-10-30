1d73e941705851ee2abea48d9c2f1e7035895780
skindervik <38836685+skindervik@users.noreply.github.com>
2021-10-30 22:31:27 +0300

    dotenv test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 function App() {
-    return <div >test</div>
+    return <div>
+        <form className="flex flex-col p-3 gap-3" method="post">
+            <input className="border" type="text" value={process.env.TEST}/>
+            <input className="border" type="password" />
+            <input type="submit" />
+        </form>
+    </div>
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import reactDom from "react-dom";
 import App from "./App";
+
 import "./index.scss";
+
 
 reactDom.render(<App/>, document.getElementById("root"));
