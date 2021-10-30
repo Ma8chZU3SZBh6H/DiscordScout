448c3363bbc41f5aabced73a690e38f969af3673
skindervik <38836685+skindervik@users.noreply.github.com>
2021-10-30 22:02:54 +0300

    auto import react test

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import React from "react";
 import reactDom from "react-dom";
+import App from "./App";
 import "./index.scss";
 
-reactDom.render(<div className="text-7xl">test</div>, document.getElementById("root"));
+reactDom.render(<App/>, document.getElementById("root"));
