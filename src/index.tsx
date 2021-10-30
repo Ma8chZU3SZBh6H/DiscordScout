0cfc93c623fd97edfe01be0a44e41f93fb963356
skindervik <38836685+skindervik@users.noreply.github.com>
2021-10-30 15:27:42 +0300

    scss test

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.scss";
 
 const test : string = "UwU";
 ReactDOM.render(<div>{test}</div>, document.getElementById("root"));
