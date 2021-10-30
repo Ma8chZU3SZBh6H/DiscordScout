f10ae0ac8d626cb80d77277e3a1913cf29f4a230
skindervik <38836685+skindervik@users.noreply.github.com>
2021-10-30 14:37:32 +0300

    swc-loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,17 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        include: /src/,
+        use: {
+          loader: "swc-loader",
+        },
+      },
+    ],
+  },
   plugins: [new HtmlWebpackPlugin()],
 };
