a6b29ef4894d6abdc2c53b593a945d61d3feba0f
skindervik <38836685+skindervik@users.noreply.github.com>
2021-11-02 13:15:09 +0200

    fixed redux dev tools

---
+++ src/frontend/index.tsx
@@ -0,0 +1,20 @@
+import reactDom from "react-dom";
+import App from "./App";
+import { compose, createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import "./index.scss";
+import reducers from "./state/reducers/reducers";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+
+const store = createStore(
+  reducers,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+reactDom.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
